189f9cf56504b823a8967f32adb0458dcc0c58e8
Diego Badelli <diego.badelli@gmail.com>
2023-08-28 16:16:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const resultDiv = document.getElementById('result');
 const backButton = document.getElementById('back-button');
 const nextButton = document.getElementById('next-button');
 const resultButton = document.getElementById('result-button');
+const resetButton = document.getElementById('reset-button'); // Botão de reiniciar
+const downloadPDFButton = document.getElementById('download-pdf-button'); // Botão de baixar PDF
 const questions = form.querySelectorAll('.question');
 const totalQuestions = questions.length;
 let currentQuestionIndex = 0;
@@ -29,13 +31,13 @@ function showResult() {
   let level = '';
   let message = '';
 
-  if (score <= 36) {
+  if (score <= 25) {
     level = 'Baixa assertividade';
     message = 'Você está usando o comportamento defensivo com muita intensidade. Isso quer dizer que, diante das diversas situações do cotidiano, você tem escolhido a passividade ou agressividade para se relacionar com as pessoas. Essas escolhas devem estar propiciando conflitos no seu dia a dia, impedindo-o de ser feliz e de equilibrar sua autoestima. Alerta!';
-  } else if (score <= 72) {
+  } else if (score <= 50) {
     level = 'Média assertividade';
     message = 'É provável que nas situações mais difíceis, nas quais se sente ameaçado, você está se armando e se defendendo com passividade ou agressividade. Em situações menos ameaçadoras, você tem conseguido ser assertivo.';
-  } else if (score <= 85) {
+  } else if (score <= 75) {
     level = 'Boa assertividade';
     message = 'Na maioria das vezes, você tem se afirmado positivamente. Porém, fique atento às situações que lhe causam desconforto. Perceba se não está perdendo em algumas situações, principalmente naquelas com pontuação menor que 3.';
   } else {
@@ -45,7 +47,28 @@ function showResult() {
 
   resultDiv.innerHTML = `<h2>Nível de Assertividade: ${level}</h2><p>${message}</p><p>Total de Pontos: ${score}</p>`;
   resultDiv.style.display = 'block';
-  resultButton.style.display = 'none';
+  resultButton.style.display = 'block';
+  resetButton.style.display = 'block'; // Mostrar o botão de reiniciar
+  downloadPDFButton.style.display = 'block'; // Mostrar o botão de baixar PDF
+}
+
+function generatePDF() {
+  const doc = new jsPDF();
+  doc.setFontSize(14);
+  doc.text('Quiz de Assertividade - Respostas', 10, 10);
+
+  let yOffset = 30;
+  questions.forEach((question, index) => {
+    const questionText = question.querySelector('p').innerText;
+    const selectedAnswer = Array.from(question.querySelectorAll('input[type="radio"]:checked')).map(input => input.parentNode.innerText).join('\n');
+    const questionScore = selectedAnswer.split(': ')[1];
+    doc.text(`Pergunta ${index + 1}: ${questionText}`, 10, yOffset);
+    doc.text(`Resposta: ${selectedAnswer}`, 10, yOffset + 10);
+    doc.text(`Pontuação: ${questionScore}`, 10, yOffset + 20);
+    yOffset += 40;
+  });
+
+  doc.save('quiz_result.pdf');
 }
 
 function goToNextQuestion() {
@@ -68,9 +91,29 @@ function goToPreviousQuestion() {
   }
 }
 
+function resetForm() {
+  questions.forEach(question => {
+    const inputs = question.querySelectorAll('input[type="radio"]');
+    inputs.forEach(input => {
+      input.checked = false;
+    });
+  });
+  resultDiv.style.display = 'none';
+  resetButton.style.display = 'none';
+  resultButton.style.display = 'none';
+  downloadPDFButton.style.display = 'none'; // Esconder o botão de baixar PDF
+  backButton.style.display = 'inline-block';
+  nextButton.style.display = 'inline-block';
+  currentQuestionIndex = 0;
+  score = 0;
+  showQuestion(currentQuestionIndex);
+}
+
 nextButton.addEventListener('click', goToNextQuestion);
 backButton.addEventListener('click', goToPreviousQuestion);
 resultButton.addEventListener('click', showResult);
+resetButton.addEventListener('click', resetForm); // Adicionar evento para o botão de reiniciar
+downloadPDFButton.addEventListener('click', generatePDF); // Adicionar evento para o botão de baixar PDF
 
 // Initialize by showing the first question
 showQuestion(currentQuestionIndex);
